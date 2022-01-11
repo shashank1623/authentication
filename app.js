7d5b10d6e56d07c37ffb57fe523456189965ec71
E.D.I.T.H <shashankbhardwaj2030@gmail.com>
2022-01-11 23:31:13 +0530

    level -3 hashing of passwords has been done

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 //in order to create user database we need to install mongoose and mongodb
 const mongoose = require("mongoose");
+const md5= require("md5");
 
-//encyption of data through mongoose encyption
-const encrypt = require("mongoose-encryption")
 const app = express();
 
 app.use(express.static("public"));
@@ -30,9 +29,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-//adding ecryption package to the schema
-
-userSchema.plugin(encrypt,{secret:process.env.SECRET , encryptedFields:['password']});
 
 
 //we can use our user schema to setup our new user model 
@@ -55,7 +51,7 @@ app.post("/register",function(req,res){
     
     const newUser = new User({
         email: req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
 
     newUser.save(function(err){   //.save field will encypt our data
@@ -70,7 +66,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email:username},function(err,foundUser){      ///.find will decrypt the data 
         if(err)
